60fa0e03bbbde824bfe4fb70dc596ca20c462b0e
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2024-06-05 18:34:59 -0500

    un-ignored config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+
+# YULATH CHANGE THIS:
+FILE_NAME = "C:/RoT/logs\eqlog_Fullstack_RoT.txt"
+
+GUILD_ID = 617825237316403207
+CHANNEL_ID = 617825237752479751
+
+parse_flag = { "run": False }
+parse_started = { "flag": False }
+
+URL = "http://45.55.129.24:8000/"
+DEV_URL = "http://127.0.0.1:8000/"
